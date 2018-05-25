313a36501370c78b8745584744e955d9d4db6c76
Alex Leonetti <leonetti.tech@gmail.com>
2018-05-25 02:10:42 -0700

    Added Registration
    Users can now register with their email address
    Display name will have to be added on activities.js if no user.displayName exists

--- source/js/pages/activities.js
+++ source/js/pages/activities.js
@@ -8,3 +8,16 @@ jQuery.noConflict();
     console.log('firebase: ', firebase.app());
   })
 })(jQuery);
+
+
+// var user = firebase.auth().currentUser;
+// user.updateProfile({
+//   displayName: name,
+// })
+// .then(function() {
+//   this.cfg.registering = false;
+//   window.location = '/pages/activities.html';
+// }.bind(this))
+// .catch(function(error) {
+//   this.handlerShowError(error.message);
+// }.bind(this));
--- source/js/pages/index.js
+++ source/js/pages/index.js
@@ -35,15 +35,20 @@ document.addEventListener('DOMContentLoaded', function(e) {
       Login = function(options) {
         this.cfg = _.extend({
           selectors : {
-            'google_login'      : '.login--google [data-google-login]',
-            'email_login'       : '.login--email [data-email-login]',
-            'login_email'       : '.login--wrapper [data-login-email]',
-            'login_password'    : '.login--wrapper [data-login-password]',
-            'error_text'        : '.error--text',
-            'error_wrapper'     : '.error--wrapper',
-            'error_close'       : '.close--error',
-          }
+            'google_login'           : '.login--google--submit [data-google-login]',
+            'email_login'            : '.login--email--submit [data-email-login]',
+            'login_email'            : '.login--wrapper [data-login-email]',
+            'login_password'         : '.login--wrapper [data-login-password]',
+            'error_text'             : '.error--text',
+            'error_wrapper'          : '.error--wrapper',
+            'error_close'            : '.close--error',
+            'register_email'         : '#register [data-register-email]',
+            'register_password'      : '#register [data-register-password]',
+            'register_p_confirm'     : '#register [data-register-password-confirm]',
+            'register_submit'        : '#register [data-register-submit]',
+          },
         })
+
         this.getDOMRefs();
         this.bindHandlers();
       };
@@ -61,7 +66,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
           // Display an error if we didn't match an element
           if (!this.$dom[name].length) {
-            this.handlerShowError('Login Developer Warning: Selected element not found');
+            console.log('Login Developer Warning: Selected element not found');
           }
         }.bind(this));
       };
@@ -76,6 +81,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
         this.$dom.google_login.on('click', this.handlerGoogleLogin.bind(this));
         this.$dom.email_login.on('click', this.handlerEmailLogin.bind(this));
         this.$dom.error_close.on('click', this.handlerCloseError.bind(this));
+        this.$dom.register_submit.on('click', this.handlerRegisterUser.bind(this));
       };
 
 
@@ -144,6 +150,43 @@ document.addEventListener('DOMContentLoaded', function(e) {
       Login.prototype.handlerShowError = function(message) {
         this.$dom.error_text.text(message);
         this.$dom.error_wrapper.show('slow');
+      };
+
+
+      /**
+        * Event that handles the user registration process
+        * onAuthStateChange handles redirect if successful
+        *
+        * @returns {Null}
+      */
+      Login.prototype.handlerRegisterUser = function(e) {
+        e.preventDefault();
+        // Local Persistence: state will be persisted even when the
+        // browser window is closed or the activity is destroyed
+        var email = this.$dom.register_email.val().trim();
+        var password = this.$dom.register_password.val();
+        var pConfirm = this.$dom.register_p_confirm.val();
+
+        if(!email.length) {
+          this.handlerShowError('Please enter your email address.');
+        } else if(!pConfirm.length || !password.length) {
+          this.handlerShowError('Please enter and confirm your password.')
+        } else if(pConfirm !== password) {
+          this.handlerShowError('Passwords do not match. Please re-enter your password now.');
+        } else if(password.length < 6 || pConfirm.length < 6) {
+          this.handlerShowError('Password must be at least 6 characters.');
+        } else {
+          firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+            .then(function() {
+              firebase.auth().createUserWithEmailAndPassword(email, password)
+                .catch(function(error) {
+                  this.handlerShowError(error.message);
+                }.bind(this));
+            }.bind(this))
+            .catch(function(error) {
+              this.handlerShowError(error.message);
+            }.bind(this));
+        }
       };
 
 
